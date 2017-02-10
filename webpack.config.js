051cd0f3af4cf4adef836a1f6e8e9fee51190698
Denys Vuika <denis.vuyka@gmail.com>
2017-02-10 19:14:30 +0000

    initial prototype

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                enforce: 'pre',
+                test: /\.tsx?$/,
+                use: "source-map-loader"
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        // contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
